073e76972c0d2522228025bff09bf3719409ae27
GuidMoreira <161589016+GuidMoreira@users.noreply.github.com>
2025-02-26 11:43:04 -0300

    script.js

---
+++ script.js
@@ -0,0 +1,7 @@
+  let numero = prompt("Digite um número") ; // exemplo de número
+
+  if (numero % 5 == 0) {
+    console.log(numero + " é múltiplo de 5!");
+  } else {
+    console.log(numero + " não é múltiplo de 5.");
+  }
